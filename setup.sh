1a44576901e4ca6c58c6b072436f415390026e15
annikaul <annika-kristin.kaul1@informatik.hs-fulda.de>
2025-02-08 16:00:47 +0100

    Rework of setup script(s)

--- setup.sh
+++ setup.sh
@@ -33,11 +33,3 @@ sudo apt install ros-humble-pcl-conversions ros-humble-sensor-msgs ros-humble-pc
 # Image saver
 sudo apt-get update
 sudo apt-get install -y ros-humble-usb-cam
-
-
-export ROSCONSOLE_FORMAT='[ROS${severity}]: ${message}'
-export LIDAR_ADDR=192.168.168.128
-export PCL_TOPIC=/ouster/points
-export IMU_TOPIC=/ouster/imu
-export RVIZ_OUSTER=true
-export RVIZ_DLIO=true
---
+++ setup_env.sh
@@ -0,0 +1,6 @@
+export ROSCONSOLE_FORMAT='[ROS${severity}]: ${message}'
+export LIDAR_ADDR=192.168.168.128
+export PCL_TOPIC=/ouster/points
+export IMU_TOPIC=/ouster/imu
+export RVIZ_OUSTER=true
+export RVIZ_DLIO=true
