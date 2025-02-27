3a396f607c8efcfe24454fdc19a0c0ecdc570af3
annikaul <annika-kristin.kaul1@informatik.hs-fulda.de>
2025-02-27 17:52:21 +0100

    Rework scripts for camera calibration and pc coloring

--- scripts/calibrate-camera.sh
+++ scripts/calibrate-camera.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
-# Run camera calibration script (from hyperspace)
+# Calibrate camera
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python3 ${SCRIPT_DIR}/../../hyperspace/hyperspace/akresearchproject/scripts/calibration.py
+
+scripts/combined-record.sh
+PID_RECORD=$!
+
+sleep 2
+declare -i i=0
+
+while i < 30; do
+    # Latest directory in sampledata directory
+    LAST_DIR=$(ls -td ${SCRIPT_DIR}/sampledata/* 2>/dev/null | head -n 1)
+
+    LAST_MOD_TIME=$(stat -c %Y "$LAST_DIR")
+    CURRENT_TIME=$(date +%s)
+    TIME_DIFF=$((CURRENT_TIME - LAST_MOD_TIME))
+
+    if [ $TIME_DIFF -le 3 ]; then
+        TARGET_DIR="${LAST_DIR}/lidar_00000000/00000002"
+        LAST_MOD_TIME=$(stat -c %Y "$TARGET_DIR")
+        CURRENT_TIME=$(date +%s)
+        TIME_DIFF=$((CURRENT_TIME - LAST_MOD_TIME))
+        if [ $TIME_DIFF -le 3 ]; then
+            kill $PID_RECORD
+            echo "Calibrate camera with new data"
+            python3 ${SCRIPT_DIR}/../../hyperspace/hyperspace/akresearchproject/scripts/calibration.py newdata
+            exit 1
+        fi
+    fi
+    i+=1
+    sleep 1
+done
+
+kill $PID_RECORD
+
+# Run camera calibration script (from hyperspace)
+echo "Calibrate camera with sampledata"
+python3 ${SCRIPT_DIR}/../../hyperspace/hyperspace/akresearchproject/scripts/calibration.py sampledata
