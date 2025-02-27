fe316d8ef4247a4931bd3b28dcd0289c2f75e6a8
annikaul <annika-kristin.kaul1@informatik.hs-fulda.de>
2025-02-27 18:37:35 +0100

    Fix variable usage

--- scripts/calibrate-camera.sh
+++ scripts/calibrate-camera.sh
@@ -3,15 +3,15 @@
 # Calibrate camera
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-scripts/combined-record.sh
+scripts/combined-record.sh &
 PID_RECORD=$!
 
 sleep 2
 declare -i i=0
 
-while i < 30; do
+while ((i < 30)); do
     # Latest directory in sampledata directory
-    LAST_DIR=$(ls -td ${SCRIPT_DIR}/sampledata/* 2>/dev/null | head -n 1)
+    LAST_DIR=$(ls -td ${SCRIPT_DIR}/../sampledata/raw/* 2>/dev/null | head -n 1)
 
     LAST_MOD_TIME=$(stat -c %Y "$LAST_DIR")
     CURRENT_TIME=$(date +%s)
@@ -29,7 +29,7 @@ while i < 30; do
             exit 1
         fi
     fi
-    i+=1
+    ((i+=1))
     sleep 1
 done
 
